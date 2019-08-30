aee103cf9bfea60e3b9ce64f827babf51632f581
Jacob Matthews <jamatthews@users.noreply.github.com>
2019-08-30 14:39:33 +0200

    tidy tests

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -1,17 +1,25 @@
 require "test_helper"
 
 class BasicTest < Minitest::Test
-  def test_trace_recorded
-    assert_equal 1002, while_loop
-  end
-
-  private
-
-  def while_loop
+  def test_basic_trace
+    trace_count = Hyperdrive.trace_count
     i = 0
     while i < 1002
       i += 1
     end
-    i
+    assert i, 1002
+    assert_equal Hyperdrive.trace_count, trace_count + 1
+  end
+
+  def test_cfunc_call
+    trace_count = Hyperdrive.trace_count
+    i = 0
+    x = [2,1]
+    while i < 1002
+      i += 1
+      x = [2,1].reverse
+    end
+    assert x = [1,2]
+    assert_equal Hyperdrive.trace_count, trace_count + 1
   end
 end
