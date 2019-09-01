fbfe555efc5f9655bc371fd391c682bcda36b3be
Jacob Matthews <jamatthews@users.noreply.github.com>
2019-09-01 19:18:19 +0200

    unfuck the specs

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -5,9 +5,9 @@ class BasicTest < Minitest::Test
     trace_count = Hyperdrive.trace_count
     i = 0
     while i < 1002
-      i += 1
+      i = i + 1
     end
-    assert i, 1002
+    assert_equal 1002, i
     assert_equal Hyperdrive.trace_count, trace_count + 1
   end
 
@@ -16,10 +16,10 @@ class BasicTest < Minitest::Test
     i = 0
     x = [2,1]
     while i < 1002
-      i += 1
+      i = i + 1
       x = [2,1].reverse
     end
-    assert x = [1,2]
+    assert_equal [1,2], x
     assert_equal Hyperdrive.trace_count, trace_count + 1
   end
 
@@ -29,7 +29,7 @@ class BasicTest < Minitest::Test
     while i < 1002
       i = add_one(i)
     end
-    assert i = 1002
+    assert_equal 1002, i
     assert_equal Hyperdrive.trace_count, trace_count + 1
   end
 
