ff2a194e9839e40a4dec065ccd2a3305e4a2a0b2
Jacob Matthews <jamatthews@users.noreply.github.com>
2019-11-22 22:42:38 +0100

    method calls are working again

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -4,28 +4,28 @@ class BasicTest < Minitest::Test
   def test_basic_trace
     trace_count = Hyperdrive.trace_count
     i = 0
-    while i < 1002
+    while i < 2000
       i = i + 1
     end
-    assert_equal 1002, i
+    assert_equal 2000, i
     assert_equal Hyperdrive.trace_count, trace_count + 1
   end
 
-  # def test_method_call
-  #   trace_count = Hyperdrive.trace_count
-  #   i = 0
-  #   while i < 1002
-  #     i = add_one(i)
-  #   end
-  #   assert_equal 1002, i
-  #   assert_equal Hyperdrive.trace_count, trace_count + 1
-  # end
+  def test_method_call
+    trace_count = Hyperdrive.trace_count
+    i = 0
+    while i < 2000
+      i = add_one(i)
+    end
+    assert_equal 2000, i
+    assert_equal Hyperdrive.trace_count, trace_count + 1
+  end
 
   def test_cfunc_call
     trace_count = Hyperdrive.trace_count
     i = 0
     x = [2,1]
-    while i < 1002
+    while i < 2000
       i = i + 1
       x = [2,1].reverse
     end
@@ -37,7 +37,7 @@ class BasicTest < Minitest::Test
     trace_count = Hyperdrive.trace_count
     i = 0
     x = 0
-    while i < 1002
+    while i < 2000
       x = 1000
       i = i + 1
     end
