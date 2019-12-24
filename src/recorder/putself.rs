2fcd489913b1c999bba8c4a3c22f02572c3d41b8
Jacob Matthews <jamatthews@users.noreply.github.com>
2019-12-24 17:24:19 +0100

    fix warning about mixing immutable and mutable borrowing

--- src/recorder/putself.rs
+++ src/recorder/putself.rs
@@ -2,7 +2,7 @@ use super::*;
 
 impl Recorder {
     pub fn record_putself(&mut self, _thread: Thread, _instruction: Instruction) {
-        let frame = &self.call_stack.last().expect("stack underflow");
-        self.stack_push(frame.self_);
+        let self_ = self.call_stack.last().expect("stack underflow").self_.clone();
+        self.stack_push(self_);
     }
 }
